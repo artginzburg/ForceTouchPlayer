a3e36876ad1881945c9a6687f8129d68defd6b66
VeloPrintemps21 <107188612+VeloPrintemps21@users.noreply.github.com>
2023-10-05 15:07:13 +0200

    Create Mario.swift

---
+++ ForceTouchPlayer/Songs/Mario.swift
@@ -0,0 +1,26 @@
+Song(
+  name: "Mario Underground Theme",
+  defaultTempo: 300.0,
+  padding: Note(frequency: 0, value: 0.15),
+  rawNotes: [
+    (NOTE_C4, 0.25), (NOTE_G4, 0.25), (NOTE_C5, 0.25), (NOTE_G4, 0.25),
+    (NOTE_C4, 0.25), (NOTE_G4, 0.25), (NOTE_C5, 0.25), (NOTE_G4, 0.25),
+    (NOTE_C4, 0.25), (NOTE_G4, 0.25), (NOTE_C5, 0.25), (NOTE_G4, 0.25),
+    (NOTE_E5, 0.5), (NOTE_C5, 0.5),
+
+    (NOTE_C4, 0.25), (NOTE_E4, 0.25), (NOTE_G4, 0.25), (NOTE_E4, 0.25),
+    (NOTE_C4, 0.25), (NOTE_E4, 0.25), (NOTE_G4, 0.25), (NOTE_E4, 0.25),
+    (NOTE_C4, 0.25), (NOTE_E4, 0.25), (NOTE_G4, 0.25), (NOTE_E4, 0.25),
+    (NOTE_A4, 0.5), (NOTE_F4, 0.5),
+
+    (NOTE_C4, 0.25), (NOTE_G4, 0.25), (NOTE_C5, 0.25), (NOTE_G4, 0.25),
+    (NOTE_C4, 0.25), (NOTE_G4, 0.25), (NOTE_C5, 0.25), (NOTE_G4, 0.25),
+    (NOTE_C4, 0.25), (NOTE_G4, 0.25), (NOTE_C5, 0.25), (NOTE_G4, 0.25),
+    (NOTE_E5, 0.5), (NOTE_C5, 0.5),
+
+    (NOTE_C4, 0.25), (NOTE_E4, 0.25), (NOTE_G4, 0.25), (NOTE_E4, 0.25),
+    (NOTE_C4, 0.25), (NOTE_E4, 0.25), (NOTE_G4, 0.25), (NOTE_E4, 0.25),
+    (NOTE_C4, 0.25), (NOTE_E4, 0.25), (NOTE_G4, 0.25), (NOTE_E4, 0.25),
+    (NOTE_A4, 0.5), (NOTE_F4, 0.5),
+  ]
+)
